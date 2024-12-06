a47d8faaf71c6417f55a08fead10a70b5403cd4a
Vincent Boulaye <vincent.boulaye@orange.com>
2024-12-06 21:27:02 +0100

    day6 cleanup

--- src/aoc2024/day06/Puzzle.kt
+++ src/aoc2024/day06/Puzzle.kt
@@ -26,51 +26,122 @@ class Puzzle {
     val part1ExpectedResult = 41
     fun part1(rawInput: List<String>): Result {
         val input = clean(rawInput)
-        val guardMoves = listOf('<', '>', 'v', '^')
-        val y = input.indices.find { i -> input[i].any { guardMoves.contains(it) } }!!
-        val x = input[y].indices.find { i ->  guardMoves.contains(input[y][i]) }!!
-        var guard = Point(x, y)
-        val moves= mutableSetOf(guard)
-        var direction=input[guard.y][guard.x]
-        while (guard.x in input[0].indices && y in input.indices) {
-            moves.add(guard)
-            val newg= when(direction) {
-                '^' -> guard.plus(Point(0,-1))
-                'v' -> guard.plus(Point(0,1))
-                '<' -> guard.plus(Point(-1,0))
-                '>' -> guard.plus(Point(1,0))
-                else -> throw Exception("wtf")
-            }
-            try {
-                when (input[newg.y][newg.x]) {
-                    '#' -> {
-                        direction = when (direction) {
-                            '^' -> '>'
-                            'v' -> '<'
-                            '<' -> '^'
-                            '>' -> 'v'
-                            else -> throw Exception("wtf")
-                        }
-                    }
-
-                    else -> guard = newg
-                }
-            } catch (e:Exception) {
-                break;
-            }
-
-        }
+        val guardDir = getGuardDirection(input)
+        val moves = getMoves(guardDir, input)
 
         return moves.size
     }
 
-    val part2ExpectedResult = 0
-    fun part2(rawInput: List<String>): Result {
-        val input = clean(rawInput)
+    data class Guard(val position: Point, val direction: Char)
 
-        return 0
+    private fun getGuardDirection(input: List<List<Char>>): Guard {
+        val guardMoves = setOf('<', '>', 'v', '^')
+        for (y in input.indices) {
+            for (x in input[y].indices) {
+                if (input[y][x] in guardMoves) {
+                    val guard = Point(x, y)
+                    val direction = input[y][x]
+                    return Guard(guard, direction)
+                }
+            }
+        }
+        throw IllegalArgumentException("Guard not found")
     }
 
+    private fun getMoves(
+        guardDir: Guard,
+        input: List<List<Char>>,
+
+        ): MutableSet<Point> {
+        var (currentPosition, currentDirection) = guardDir
+        val moves = mutableSetOf<Point>()
+        while (true) {
+            moves.add(currentPosition)
+            val newPosition = when (currentDirection) {
+                '^' -> currentPosition + Point(0, -1)
+                'v' -> currentPosition + Point(0, 1)
+                '<' -> currentPosition + Point(-1, 0)
+                '>' -> currentPosition + Point(1, 0)
+                else -> throw Exception("wtf")
+            }
+            if (newPosition.y !in input.indices
+                || newPosition.x !in input[newPosition.y].indices
+            ) {
+                return moves
+            }
+            when (input[newPosition.y][newPosition.x]) {
+                '#' -> {
+                    currentDirection = when (currentDirection) {
+                        '^' -> '>'
+                        'v' -> '<'
+                        '<' -> '^'
+                        '>' -> 'v'
+                        else -> throw Exception("wtf")
+                    }
+                }
+
+                else -> currentPosition = newPosition
+            }
+
+        }
+    }
+
+    val part2ExpectedResult = 6
+    fun part2(rawInput: List<String>): Result {
+        val input = clean(rawInput)
+        val guardDir = getGuardDirection(input)
+        val moves = getMoves(guardDir, input)
+
+        return moves
+            .filter { bloc -> bloc != guardDir.position }
+            .filter { bloc -> getMoves2(bloc, guardDir, input) }
+            .count()
+    }
+
+    private fun getMoves2(
+        bloc: Point,
+        guardDir: Guard,
+        inputRaw: List<List<Char>>,
+
+        ): Boolean {
+
+        val input = inputRaw.map { it.toMutableList() }
+        input[bloc.y][bloc.x] = 'O'
+        var (currentPosition, currentDirection) = guardDir
+        val moves = mutableSetOf<Pair<Point, Char>>()
+        while (true) {
+            if (!moves.add(currentPosition to currentDirection)) {
+                return true
+            }
+            val newPosition = when (currentDirection) {
+                '^' -> currentPosition + Point(0, -1)
+                'v' -> currentPosition + Point(0, 1)
+                '<' -> currentPosition + Point(-1, 0)
+                '>' -> currentPosition + Point(1, 0)
+                else -> throw Exception("wtf")
+            }
+            if (newPosition.y !in input.indices
+                || newPosition.x !in input[newPosition.y].indices
+            ) {
+                return false
+            }
+            when (input[newPosition.y][newPosition.x]) {
+                '#', 'O' -> {
+                    currentDirection = when (currentDirection) {
+                        '^' -> '>'
+                        'v' -> '<'
+                        '<' -> '^'
+                        '>' -> 'v'
+                        else -> throw Exception("wtf")
+                    }
+                }
+
+                else -> currentPosition = newPosition
+            }
+
+
+        }
+    }
 }
 
 
