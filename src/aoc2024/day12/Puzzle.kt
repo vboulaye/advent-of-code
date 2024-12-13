15c3a8fca259a4326b29182fc2ff4e3fa11e7378
Vincent Boulaye <vincent.boulaye@orange.com>
2024-12-13 09:07:17 +0100

    day12 wip

--- src/aoc2024/day12/Puzzle.kt
+++ src/aoc2024/day12/Puzzle.kt
@@ -1,5 +1,8 @@
 package aoc2024.day12
 
+import utils.Point
+import utils.browsePoints
+import utils.getPoint
 import utils.readInput
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
@@ -12,28 +15,181 @@ class Puzzle {
 
     fun String.acceptInput() = true
 
-    fun String.parseInput(): String {
-        return this
+    fun String.parseInput(): List<Char> {
+        return this.toList()
     }
 
-    fun clean(input: List<String>): List<String> {
+    fun clean(input: List<String>): List<List<Char>> {
         return input
             .filter { line -> line.acceptInput() }
             .map { line -> line.parseInput() }
     }
 
-    val part1ExpectedResult = 0
+    val part1ExpectedResult = 1930
     fun part1(rawInput: List<String>): Result {
         val input = clean(rawInput)
+        val seen = mutableSetOf<Point>()
+        val areaPerimeter = mutableMapOf<Point, Pair<Int, Int>>()
+        val aretoToPerimeter = input.browsePoints()
+            .filter { (p, v) -> !(p in seen) }
+            .mapNotNull { (p, v) ->
+                if (!(p in seen)) {
+                    println(p)
+                    val seen2 = mutableSetOf<Point>()
+                    val areaPoints = computeAp(p, v, input, seen)
+                    val points = areaPoints
+                    println(v + "=" + points)
+                    points
+                } else {
+                    null
+                }
+            }.sumOf { areaPoints ->
+                val perimeter = areaPoints.sumOf { p ->
+                    p.neighbours()
+                        .count { it !in areaPoints }
+                }
+                println(areaPoints.size.toString() + '*' + perimeter)
+                areaPoints.size * perimeter
+            }
 
-        return 0
+        return aretoToPerimeter
     }
 
-    val part2ExpectedResult = 0
+    private fun computeAp(
+        p: Point,
+        v: Char,
+        input: List<List<Char>>,
+        seen2: MutableSet<Point>,
+    ): List<Point> {
+        seen2.add(p)
+        return listOf(p) + p.neighbours()
+            .filter { input.getPoint(it, '~') == v }
+            .flatMap {
+                if (!(it in seen2)) {
+
+                    val computeAp = computeAp(it, v, input, seen2)
+                    computeAp
+                } else {
+                    emptyList()
+                }
+
+            }
+    }
+
+    val part2ExpectedResult = 1206
     fun part2(rawInput: List<String>): Result {
         val input = clean(rawInput)
+        val seen = mutableSetOf<Point>()
+        val areaPerimeter = mutableMapOf<Point, Pair<Int, Int>>()
+        val aretoToPerimeter = input.browsePoints()
+            .filter { (p, v) -> !(p in seen) }
+            .mapNotNull { (p, v) ->
+                if (!(p in seen)) {
+                    println(p)
+                    val seen2 = mutableSetOf<Point>()
+                    val areaPoints = computeAp(p, v, input, seen)
+                    val points = areaPoints
+                    println(v + "=" + points)
+                    points
+                } else {
+                    null
+                }
+            }.sumOf { areaPoints ->
+//                val distinct = areaPoints
+//                    .flatMap { p ->
+//                        p.neighbours()
+//                            .filter { it !in areaPoints }
+//                            .map {
+//                                val direction = it - p
+//                                val serializable = when {
+//                                    direction == Point(1, 0) -> p.x to p.x + 1 // '>'
+//                                    direction == Point(-1, 0) -> p.x - 1 to p.x//'<'
+//                                    direction == Point(0, 1) -> 1000000 * (p.y) to 1000000 * (p.y+ 1) //'v'
+//                                    direction == Point(0, -1) -> 1000000 * (p.y - 1) to 1000000 * (p.y) // '^'
+//                                    else -> Error("unexpected direction $direction")
+//                                }
+//                                serializable
+//                            }
+//                    }
+//                    .distinct()
+                val distinct = areaPoints
+                    .flatMap { p ->
+                        p.neighbours()
+                            .filter { it !in areaPoints }
+                            .map {
+//                                val direction = it - p
+//                                val serializable = when {
+//                                    direction == Point(1, 0) -> p.x to p.x + 1 // '>'
+//                                    direction == Point(-1, 0) -> p.x - 1 to p.x//'<'
+//                                    direction == Point(0, 1) -> 1000000 * (p.y) to 1000000 * (p.y+ 1) //'v'
+//                                    direction == Point(0, -1) -> 1000000 * (p.y - 1) to 1000000 * (p.y) // '^'
+//                                    else -> Error("unexpected direction $direction")
+//                                }
+                                p to it
+                            }
+                    }
+                    .toMutableList()
+                var i = 0
+                while (i < distinct.size) {
+                    val (from, to) = distinct[i]
+                    val seen1 = mutableSetOf<Pair<Point, Point>>()
+                    val other = getStraightLine(distinct[i], distinct, seen1)
+                        .toMutableList()
+//                    other.remove(distinct[i])
+//                    distinct.removeAll(other)
+                    seen1.remove(distinct[i])
+                    distinct.removeAll(seen1)
+                    i++
+                }
+                println(areaPoints + "=>" + distinct)
+                val perimeter = distinct
+                    .count()
 
-        return 0
+                println(areaPoints.size.toString() + '*' + perimeter)
+                areaPoints.size * perimeter
+            }
+
+        return aretoToPerimeter
+    }
+
+    private fun getStraightLine(
+        pair: Pair<Point, Point>,
+        distinct: MutableList<Pair<Point, Point>>,
+        seen: MutableSet<Pair<Point, Point>>
+    ): List<Pair<Point, Point>> {
+        val direction = pair.second - pair.first
+
+        val serializable = when {
+            direction.y == 0 -> distinct
+                .filter {
+                    val direction2 = it.second - it.first
+                    val b = !(it in seen) && direction2.y == 0 && (
+                            (it.second.x == pair.second.x && it.first.x == pair.first.x)
+                                    || (it.second.x == pair.first.x && it.first.x == pair.second.x)
+                            )
+                    b
+                }
+                .flatMap {
+                    seen.add(it)
+                    listOf(it) + getStraightLine(it, distinct, seen)
+                }
+
+            direction.x == 0 -> distinct
+                .filter {
+                    val direction2 = it.second - it.first
+                    !(it in seen) && direction2.x == 0 && (
+                            (it.second.y == pair.second.y && it.first.y == pair.first.y)
+                                    || (it.second.y == pair.first.y && it.first.y == pair.second.y)
+                            )
+                }
+                .flatMap {
+                    seen.add(it)
+                    listOf(it) + getStraightLine(it, distinct, seen)
+                }
+
+            else -> throw Error("unexpected direction $direction")
+        }
+        return serializable
     }
 
 }
@@ -60,7 +216,7 @@ fun main() {
         println("${part}: test took ${testDuration.inWholeMilliseconds}ms, full took ${fullDuration.inWholeMilliseconds}ms")
     }
 
-    runPart("part1", puzzle.part1ExpectedResult, puzzle::part1)
+//    runPart("part1", puzzle.part1ExpectedResult, puzzle::part1)
     runPart("part2", puzzle.part2ExpectedResult, puzzle::part2)
 
 }
