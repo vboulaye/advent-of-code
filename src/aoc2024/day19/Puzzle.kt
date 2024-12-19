c3f9c8d9f91dc4e480d10326da815ad5e39b411e
Vincent Boulaye <vincent.boulaye@orange.com>
2024-12-19 08:29:41 +0100

    day19

--- src/aoc2024/day19/Puzzle.kt
+++ src/aoc2024/day19/Puzzle.kt
@@ -16,24 +16,91 @@ class Puzzle {
         return this
     }
 
-    fun clean(input: List<String>): List<String> {
-        return input
-            .filter { line -> line.acceptInput() }
-            .map { line -> line.parseInput() }
+    fun clean(input: List<String>): Game {
+        var patterns = listOf("")
+        var designs = listOf<String>()
+
+        input
+            .filter { line -> line.isNotEmpty() }
+            .map { line ->
+                when {
+                    line.contains(", ") -> patterns = line.split(", ")
+                    else -> designs += line
+                }
+            }
+        return Game(patterns, designs)
     }
 
-    val part1ExpectedResult = 0
+    data class Game(val patterns: List<String>, val designs: List<String>)
+
+    val part1ExpectedResult = 6
     fun part1(rawInput: List<String>): Result {
         val input = clean(rawInput)
 
-        return 0
+        val r = Regex("^(" + input.patterns.joinToString("|") + ")+$")
+        return input.designs.count { design ->
+            r.matches(design)
+        }
     }
 
-    val part2ExpectedResult = 0
+    val part2ExpectedResult = 16
     fun part2(rawInput: List<String>): Result {
         val input = clean(rawInput)
+        val r = Regex("^(" + input.patterns.joinToString("|") + ")+$")
+        val r2 = Regex("(" + input.patterns.joinToString("|") + ")")
+        return input.designs
+            .filter { design -> r.matches(design) }
+            .sumOf { design ->
+                print("\n\n\ncount($design) = \n")
+                val d = decompose(design, input.patterns)
+//                val count = count(r, design, r2, input.patterns,0)
+                d.size
+            }
+    }
 
-        return 0
+    val cache = mutableMapOf<String, List<List<String>>>()
+
+    private fun decompose(design: String, patterns: List<String>): List<List<String>> {
+        if (design.isEmpty()) return listOf(listOf())
+        cache.get(design)?.let {
+//            println("cache hit")
+            return it }
+        val flatMap = patterns
+            .filter { design.startsWith(it) }
+            .flatMap { firstPattern ->
+                decompose(design.replaceFirst(firstPattern, ""), patterns)
+                    .map { listOf(firstPattern) + it }
+            }
+        cache.put(design, flatMap)
+        return flatMap
+//            .map { firstPattern => decompose( design.replaceFirst(firstPattern, ""), patterns)
+//                    .flatMap { l => listOf(it ) + l } }
+    }
+
+    val m = mutableMapOf<String, Regex>()
+    private fun count(r: Regex, design: String, r2: Regex, patterns: List<String>, level: Int): Int {
+        if (design.isEmpty()) {
+            //println(" ".repeat(level) + "count($design) = 1")
+            return 1
+        }
+        val sumOf = patterns
+            .filter { design.startsWith(it) }
+            .sumOf { pattern ->
+                val count = count(r, design.replaceFirst(pattern, "").removePrefix(pattern), r2, patterns, level + 1)
+                println(" ".repeat(level) + "count($design) = $count")
+                if (count == 0) 0 else count
+            }
+        return sumOf
+//        if (design.length == 1) return 1
+//        val r3 = Regex("(" + patterns.joinToString("|") + ")")
+//        if (!r3.matches(design)) return 0
+//        return 1 + r3.findAll(design)
+//            .sumOf { matchResult ->
+//                val count = count(r, matchResult!!.value, r2, patterns.filter { it != matchResult!!.value })
+//                if (count == 0) return 0
+//                count
+//            }
+
     }
 
 }
