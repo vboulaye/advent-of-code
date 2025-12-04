bade0a12c8160e8149a8fbbec619a6e8f1427c22
Vincent Boulaye <vincent.boulaye@orange.com>
2025-12-04 07:33:02 +0100

    2025 day 4

--- src/aoc2025/day04/Puzzle.kt
+++ src/aoc2025/day04/Puzzle.kt
@@ -1,5 +1,6 @@
 package aoc2025.day04
 
+import org.jetbrains.kotlinx.multik.ndarray.complex.Complex.Companion.i
 import utils.readInput
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
@@ -11,24 +12,73 @@ typealias Result2 = Int
 class Puzzle {
 
 
-    fun parseInput(rawInput: List<String>): List<String> {
+    fun parseInput(rawInput: List<String>): MutableList<MutableList<Char>> {
         return rawInput
             .filter { line -> true }
-            .map { line -> line }
+            .map { line -> line.toMutableList() }.toMutableList()
     }
 
-    val part1ExpectedResult: Result1 = 0
+    val part1ExpectedResult: Result1 = 13
+    private fun count(x: Int, y: Int, input: List<List<Char>>): Int {
+
+        var c = 0
+        for (dy in -1..1) {
+            for (dx in -1..1) {
+                if (dx == 0 && dy == 0) continue
+                var nx = x + dx
+                var ny = y + dy
+                if (ny in input.indices && nx in input[ny].indices) {
+                    if (input[ny][nx] == '@') {
+                        c++
+                    }
+                }
+            }
+        }
+        return c
+    }
+
     fun part1(rawInput: List<String>): Result1 {
         val input = parseInput(rawInput)
-
-        return 0
+        var result = 0
+        for (y in input.indices) {
+            val line = input[y]
+            // Implement the logic for part 1 here
+            for (x in line.indices) {
+                if (input[y][x] == '@' && count(x, y, input) < 4) {
+                    println("found at $y,$x")
+                    result++
+                }
+            }
+        }
+        return result
     }
 
-    val part2ExpectedResult: Result2 = 0
+    val part2ExpectedResult: Result2 = 43
     fun part2(rawInput: List<String>): Result2 {
         val input = parseInput(rawInput)
+        var result = 0
+        var round: Int=-1
+        while(round!=0) {
+            round = removePossible(input)
+            result+=round
+        }
+        return result
+    }
 
-        return 0
+    private fun removePossible(input: MutableList<MutableList<Char>>): Int {
+        var result1 = 0
+        for (y in input.indices) {
+            val line = input[y]
+            // Implement the logic for part 1 here
+            for (x in line.indices) {
+                if (input[y][x] == '@' && count(x, y, input) < 4) {
+                    println("found at $y,$x")
+                    input[y][x]= 'x'
+                    result1++
+                }
+            }
+        }
+        return result1
     }
 
 }
