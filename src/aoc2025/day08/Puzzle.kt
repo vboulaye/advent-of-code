c0ab27554e035800388585ec1aa81a0e7e8f2f9b
Vincent Boulaye <vincent.boulaye@orange.com>
2025-12-07 10:14:21 +0100

    2025 day 8 init

---
+++ src/aoc2025/day08/Puzzle.kt
@@ -0,0 +1,65 @@
+package aoc2025.day08
+
+import utils.readInput
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+typealias Result1 = Long
+typealias Result2 = Long
+
+
+class Puzzle {
+
+
+    fun parseInput(rawInput: List<String>): List<String> {
+        return rawInput
+            .filter { line -> true }
+            .map { line -> line }
+    }
+
+    val part1ExpectedResult: Result1 = 0L
+    fun part1(rawInput: List<String>): Result1 {
+        val input = parseInput(rawInput)
+
+        var result = 0L
+
+        return result
+    }
+
+    val part2ExpectedResult: Result2 = 0L
+    fun part2(rawInput: List<String>): Result2 {
+        val input = parseInput(rawInput)
+
+        var result = 0L
+
+        return result
+    }
+
+}
+
+
+@OptIn(ExperimentalTime::class)
+fun main() {
+    val puzzle = Puzzle()
+    println(Puzzle::class.qualifiedName)
+
+    val testInput = readInput("00test", Puzzle::class)
+    val input = readInput("zzdata", Puzzle::class)
+
+    fun <R> runPart(part: String, expectedTestResult: R, partEvaluator: (List<String>) -> R) {
+        val testDuration = measureTime {
+            val testResult = partEvaluator(testInput)
+            println("test ${part}: $testResult == ${expectedTestResult}")
+            check(testResult == expectedTestResult) { "$testResult != ${expectedTestResult}" }
+        }
+        val fullDuration = measureTime {
+            val fullResult = partEvaluator(input)
+            println("${part}: $fullResult")
+        }
+        println("${part}: test took ${testDuration.inWholeMilliseconds}ms, full took ${fullDuration.inWholeMilliseconds}ms")
+    }
+
+    runPart("part1", puzzle.part1ExpectedResult, puzzle::part1)
+    runPart("part2", puzzle.part2ExpectedResult, puzzle::part2)
+
+}
